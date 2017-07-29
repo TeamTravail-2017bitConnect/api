4d7bda1bc9ea76e387098b2128357bf0b4ac0d22
Yuto Suzuki <yuutoo.advance@gmail.com>
2017-07-30 01:02:41 +0900

    add location history

--- app/controller/truckController.py
+++ app/controller/truckController.py
@@ -4,6 +4,10 @@ import pymongo
 import utils
 import baggageController as bc
 
+bagKey = "baggages"
+doneKey = "done"
+deliverKey = "delivering"
+
 class TruckController(object):
     
     def __init__(self, db):
@@ -12,16 +16,27 @@ class TruckController(object):
     def on_get(self, req, resp, truck_id):
         trucks = self.db.trucks.find({"id": float(truck_id)})
         truck = [data for data in trucks][0]
+        truck = self.createBaggage(truck)
+        del truck["_id"]
+        del truck["baggageIds"]
+        truck = self.createHistory(truck)
+        
+        truck["progressRate"] = float(len(truck[bagKey][doneKey]))/(len(truck[bagKey][doneKey]) + len(truck[bagKey][deliverKey]))
+        resp.body = json.dumps(truck, ensure_ascii=False)
+    
+    def createBaggage(self, truck):
+ 
         baggageIds = [{"baggageId":data} for data in truck['baggageIds']]
         baggages = self.db.baggages.find({"$or": baggageIds})
         bags = bc.BaggageController.toData(baggages)
-        del truck["_id"]
-        
-        bagKey = "baggages"
-        doneKey = "done"
-        deliverKey = "delivering"
+
         truck[bagKey] = {}
         truck[bagKey][doneKey] = [b for b in bags if b["status"]==doneKey]
         truck[bagKey][deliverKey] = [b for b in bags if b["status"]==deliverKey]
-        truck["progressRate"] = float(len(truck[bagKey][doneKey]))/(len(truck[bagKey][doneKey]) + len(truck[bagKey][deliverKey]))
-        resp.body = json.dumps(truck, ensure_ascii=False)
+        return truck
+
+    def createHistory(self, truck):
+        id = truck["id"]
+        history = self.db.location_logs.find({"truckId":float(id)}).limit(20)
+        truck["locationHistory"] = [utils.removeId(x) for x in history]
+        return truck
