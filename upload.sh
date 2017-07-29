a9a6cfd4c601b1de7b58ae2550baf2a764ba9821
Yuto Suzuki <yuutoo.advance@gmail.com>
2017-07-29 20:59:25 +0900

    fix upload.sh

--- upload.sh
+++ upload.sh
@@ -1,9 +1,12 @@
 #! /bin/sh
+HOME=/home/ec2-user
+GUNICORN=$HOME/.pyenv/shims/gunicorn
+PID=$HOME/PID
 
 zip -r app.zip app
-scp -i ~/Downloads/travail.pem app.zip ec2-user@52.192.108.213:~/travail/
-ssh -i ~/Downloads/travail.pem ec2-user@52.192.108.213 "kill `cat PID`"
-ssh -i ~/Downloads/travail.pem ec2-user@52.192.108.213 "unzip -o app.zip -d travail"
-ssh -i ~/Downloads/travail.pem ec2-user@52.192.108.213 "gunicorn -p PID travail.main:app&"
+scp -i travail.pem app.zip ec2-user@52.192.108.213:$HOME/travail/
+ssh -i travail.pem ec2-user@52.192.108.213 "kill `cat PID`"
+ssh -i travail.pem ec2-user@52.192.108.213 "unzip -o app.zip -d travail"
+ssh -i travail.pem ec2-user@52.192.108.213 exec $GUNICORN -p $PID travail.main:app&
 
 
