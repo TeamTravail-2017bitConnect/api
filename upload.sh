329cb15fdea805b8ea07dd80b05621204c64d87a
Yuto Suzuki <yuutoo.advance@gmail.com>
2017-07-29 23:33:38 +0900

    fix upload.sh

--- upload.sh
+++ upload.sh
@@ -5,11 +5,12 @@ PID=$HOME/PID
 cmd=`cat $PID`
 errorlog=/var/log/gunicorn/error-log
 
+rm app.zip
 zip -r app.zip app/*
-scp -i travail.pem app.zip ec2-user@52.192.108.213:$HOME/travail/
+scp -i travail.pem app.zip ec2-user@52.192.108.213:$HOME/app.zip
+ssh -i travail.pem ec2-user@52.192.108.213 "rm -rf travail"
 ssh -i travail.pem ec2-user@52.192.108.213 sh stop.sh
-ssh -i travail.pem ec2-user@52.192.108.213 "rm -rf $HOME/travail"
-ssh -i travail.pem ec2-user@52.192.108.213 "unzip -o app.zip -d travail"
-# ssh -i travail.pem ec2-user@52.192.108.213 sh start.sh
+ssh -i travail.pem ec2-user@52.192.108.213 unzip -o app.zip -d travail
+#ssh -i travail.pem ec2-user@52.192.108.213 sh load.sh
 
 
